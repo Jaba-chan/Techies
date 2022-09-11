0ce47da3fbd6ce0877614bbb27e8ffa351987b38
kuzak <kuzakovevgen@gmail.com>
2022-09-11 14:32:13 +0700

    1

--- Techies/Form1.cs
+++ Techies/Form1.cs
@@ -19,129 +19,41 @@ namespace Techies
         static int net_height = 16;
         static int net_weight = 30;
         static int b_size = 20;
-        int[] black_list = new int[bomb_amount+1];
-        int[] weight_point = new int[net_height * net_weight];
-        int[] NET = new int[net_weight * net_height];
-        static Button[] buttoms_array = new Button[net_weight * net_height];
-        void Create_Weight()
+        Point[] black_list = new Point[bomb_amount+1];
+        Point[] NET = new Point[net_weight * net_height];
+        int[,] weight_point = new int[net_weight , net_height];
+        static Button[,] buttoms_array = new Button[net_weight , net_height];
+        void Mine(Button trigered)
         {
-            for (int i = 0; i < buttoms_array.Length; i++)
-                if (!black_list.Contains(i))
-                {
-                    buttoms_array[i].Text = weight_point[i].ToString();
-                }
-        }
-        void Mine()
-        {
-            int[] right_side =  new int[net_height];
-            for (int j = 0; j < net_height; j++)
-                {
-                    right_side[j] = net_weight - 1 + net_weight * j;
-                }
+            black_list[0] = new Point(trigered.Location.X / net_weight, trigered.Location.Y / net_height);
             Random rnd = new Random();
             for (int i = 1; i < bomb_amount + 1; i++)
             {
-                var free_for_mine = NET.Except(black_list).ToArray();
-                black_list[i] = free_for_mine[rnd.Next(0, free_for_mine.Length - 1)];
-                int index_w = black_list[i];
-                
-                
-                if (index_w % net_weight == 0 && index_w != 0 && index_w != (net_height - 1) * net_weight)
-                {
-                    weight_point[index_w + 1] += 1;
-                    weight_point[index_w - net_weight] += 1;
-                    weight_point[index_w + net_weight] += 1;
-                    weight_point[index_w + net_weight + 1] += 1;
-                    weight_point[index_w - net_weight - 1] += 1;
-                }
-                else if (right_side.Contains(index_w) && index_w != (net_weight - 1) && index_w != net_height * net_weight - 1)
-                {
-                    weight_point[index_w - 1] += 1;
-                    weight_point[index_w - net_weight] += 1;
-                    weight_point[index_w + net_weight] += 1;
-                    weight_point[index_w - net_weight + 1] += 1;
-                    weight_point[index_w + net_weight - 1] += 1;
-                }
-                else if (index_w < net_weight - 1 && index_w > 0)
-                {
-                    weight_point[index_w + 1] += 1;
-                    weight_point[index_w - 1] += 1;
-                    weight_point[index_w + net_weight] += 1;
-                    weight_point[index_w + net_weight - 1] += 1;
-                    weight_point[index_w + net_weight + 1] += 1;
-                }
-                else if (index_w < net_weight * net_height - 1 && index_w > net_weight * (net_height - 1))
-                {
-                    weight_point[index_w + 1] += 1;
-                    weight_point[index_w - 1] += 1;
-                    weight_point[index_w - net_weight] += 1;
-                    weight_point[index_w - net_weight - 1] += 1;
-                    weight_point[index_w - net_weight + 1] += 1;
-                }
-                else if (index_w == 0)
-                {
-                    weight_point[index_w + 1] += 1;
-                    weight_point[index_w + net_weight] += 1;
-                    weight_point[index_w + net_weight + 1] += 1;
-                }
-                else if (index_w == net_weight - 1)
-                {
-                    weight_point[index_w - 1] += 1;
-                    weight_point[index_w + net_weight] += 1;
-                    weight_point[index_w + net_weight - 1] += 1;
-                }
-                else if (index_w == (net_height - 1) * net_weight)
-                {
-                    weight_point[index_w + 1] += 1;
-                    weight_point[index_w - net_weight] += 1;
-                    weight_point[index_w - net_weight - 1] += 1;
-                }
-                else if (index_w == (net_height * net_weight - 1))
-                {
-                    weight_point[index_w - 1] += 1;
-                    weight_point[index_w - net_weight] += 1;
-                    weight_point[index_w - net_weight + 1] += 1;
-                }
-                else 
-                {
-                    weight_point[index_w + 1] += 1;
-                    weight_point[index_w - 1] += 1;
-                    weight_point[index_w - net_weight] += 1;
-                    weight_point[index_w + net_weight] += 1;
-                    weight_point[index_w - net_weight + 1] += 1;
-                    weight_point[index_w - net_weight - 1] += 1;
-                    weight_point[index_w + net_weight + 1] += 1;
-                    weight_point[index_w + net_weight - 1] += 1;
-                    
-                }
-            }
-            for (int i = 1; i < bomb_amount + 1; i++)
-            {
-                buttoms_array[black_list[i]].BackColor = Color.Red;
-            }
-        }
-        
-        void button_Clicked(object sender, EventArgs e)
-        {
-            Button trigered_button = (Button)sender;
-            Debug.WriteLine(trigered_button.Name);
-            if (Start_Game == false)
-            {   
-                black_list[0] =Int32.Parse(trigered_button.Name);
-                Start_Game = true;
-                Mine();
-                Create_Weight();
+                var segment = NET.Except<Point>(black_list).ToArray<Point>();
+                int new_mine_location = rnd.Next(0, segment.Length);
+                black_list[i] = segment[new_mine_location];
+                buttoms_array[black_list[i].X, black_list[i].Y].BackColor = Color.Red;
             }
 
+        }
+        void button_Clicked(object sender, EventArgs e)
+        {
+           
+            Button trigered_button = (Button)sender;
+            if (Start_Game == false)
+            {
+                Mine(trigered_button);
+            }
         }
         void Make_Button(int x, int y, int i)
         {
             Button button = new Button();
             button.Name = i.ToString();
             button.Size = new Size(b_size, b_size);
-            buttoms_array[i] = button;
-            button.Location = new Point(x, y);
+            buttoms_array[x, y] = button;
+            button.Location = new Point(x*b_size, y*b_size);
             button.Click += new System.EventHandler(this.button_Clicked);
+            button.BackColor = Color.FromArgb(255, 211, 211, 211);
             this.Controls.Add(button);
         }
         
@@ -157,8 +69,10 @@ namespace Techies
             {
                 for (int j = 0 ; j < net_height; j++)
                 {
-                    Make_Button(i*b_size, j*b_size, j*net_weight + i);
-                    NET[j * net_weight + i] = j * net_weight + i;
+                    Make_Button(i, j, j*net_weight + i);
+                    weight_point[i, j] = 0;
+                    NET[net_weight * j + i] = new Point(i, j);
+                    
                 }
             }
         }
