6a596eb57572488a0063ffd79c3507f3c3b93988
kuzak <kuzakovevgen@gmail.com>
2022-09-12 21:48:04 +0700

    1

--- Techies/Form1.cs
+++ Techies/Form1.cs
@@ -14,27 +14,49 @@ namespace Techies
     
     public partial class Form1 : Form
     {
+        static int indents = 20;
+        Image cell = Image.FromFile("C:/Users/kuzak/source/repos/Techies/Techies/Properties/cell.png");
+        Image m_r = Image.FromFile("C:/Users/kuzak/source/repos/Techies/Techies/Properties/M_R.png");
+        Image m_g = Image.FromFile("C:/Users/kuzak/source/repos/Techies/Techies/Properties/M_G.png");
+        Image[] Pictures_array = new Image[9];
         static int bomb_amount = 99;
         static bool Start_Game = false;
         static int net_height = 16;
         static int net_weight = 30;
-        static int b_size = 20;
+        static int b_size = 30;
         Point[] black_list = new Point[bomb_amount+1];
         Point[] NET = new Point[net_weight * net_height];
         int[,] weight_point = new int[net_weight , net_height];
         static Button[,] buttoms_array = new Button[net_weight , net_height];
         bool [,] is_cell_opened = new bool[net_weight , net_height];
 
-
+        void Lose(Button mine)
+        {
+            
+            for (int i = 1; i <= bomb_amount; i++)
+            {
+                int x_mime = black_list[i].X;
+                int y_mine = black_list[i].Y;
+                buttoms_array[x_mime, y_mine].BackgroundImage = m_g; 
+            }
+            mine.BackgroundImage = m_r;
+            MessageBox.Show("Ихихихи");
+        }
         void Open_Cell(int X, int Y, int previous_weight)
         {
-            if (is_cell_opened[X, Y] == false && previous_weight != 0)
+            if (weight_point[X, Y] < 0)
+            { Lose(buttoms_array[X, Y]);}
+            else
             {
-                buttoms_array[X, Y].BackColor = Color.FromArgb(255,255,255,255);
-                is_cell_opened[X, Y] = true;
-                previous_weight = weight_point[X, Y];
-                return;
+                if (is_cell_opened[X, Y] == false && previous_weight != 0)
+                {
+                    buttoms_array[X, Y].BackgroundImage = Pictures_array[weight_point[X, Y]];
+                    is_cell_opened[X, Y] = true;
+                    previous_weight = weight_point[X, Y];
+                    return;
+                }
             }
+            
             for (int i = - 1; i <= 1; i++)
             {
                 for (int j = - 1; j <=1; j++)
@@ -45,14 +67,14 @@ namespace Techies
                         if (is_cell_opened[X + i, Y + j] == false && weight_point[X + i, Y + j] == 0)
                         {
                             is_cell_opened[X + i, Y + j] = true;
-                            buttoms_array[X + i, Y + j].BackColor = Color.FromArgb(255, 255, 255, 255);
+                            buttoms_array[X + i, Y + j].BackgroundImage = Pictures_array[weight_point[X + i, Y + j]];
                             previous_weight = weight_point[X + i, Y + j];
                             Open_Cell(X + i, Y + j, previous_weight);
                         }
                         else if (is_cell_opened[X + i, Y + j] == false && previous_weight == 0)
                         {
                             is_cell_opened[X + i, Y + j] = true;
-                            buttoms_array[X + i, Y + j].BackColor = Color.FromArgb(255, 255, 255, 255);
+                            buttoms_array[X + i, Y + j].BackgroundImage = Pictures_array[weight_point[X + i, Y + j]];
                             previous_weight = weight_point[X, Y];
                         }   
                     }
@@ -61,23 +83,6 @@ namespace Techies
             }
         }
 
-        void Show_Weight()
-        {
-            for (int i = 0; i < net_weight; i++)
-            {
-                for (int j = 0; j < net_height; j++)
-                {
-                    if (!black_list.Contains<Point>(new Point(i, j)))
-                        {
-                        buttoms_array[i,j].Text = weight_point[i, j].ToString();
-                        }
-                    else if (black_list[0].X == i && black_list[0].Y == j)
-                        {
-                        buttoms_array[i, j].Text = weight_point[i, j].ToString();
-                        }
-                }
-            }
-        }
         void Mine(Button trigered)
         {
             string[] first_click_str = trigered.Name.Split(' ');
@@ -92,7 +97,8 @@ namespace Techies
                 black_list[i] = segment[new_mine_location];
                 int X = black_list[i].X;
                 int Y = black_list[i].Y;
-                buttoms_array[X, Y].BackColor = Color.Red;
+                //buttoms_array[X, Y].BackgroundImage = Pictures_array[0];
+                weight_point[X, Y] = -100;
                 for (int j = - 1; j <= 1; j++)
                 {for (int z = -1; z <= 1; z++)
                     {try {weight_point[X + j, Y + z] += 1;}
@@ -113,7 +119,6 @@ namespace Techies
             if (Start_Game == false)
             {
                 Mine(trigered_button);
-                Show_Weight();
                 Start_Game = true;
             }
             int click_weight = weight_point[X, Y]; 
@@ -123,11 +128,16 @@ namespace Techies
         {
             Button button = new Button();
             button.Name = i.ToString();
+            button.BackgroundImage = cell;
+            button.BackgroundImageLayout = ImageLayout.Stretch;
             button.Size = new Size(b_size, b_size);
             buttoms_array[x, y] = button;
-            button.Location = new Point(x*b_size, y*b_size);
+            button.Location = new Point(x*b_size + indents, y*b_size + indents);
             button.Click += new System.EventHandler(this.button_Clicked);
             button.BackColor = Color.FromArgb(255, 200, 200, 200);
+            button.FlatStyle = FlatStyle.Flat;
+            button.FlatAppearance.BorderSize = 1;
+            button.FlatAppearance.BorderColor = Color.FromArgb(255, 150, 150, 150);
             this.Controls.Add(button);
         }
         
@@ -138,7 +148,12 @@ namespace Techies
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            this.ClientSize = new Size(net_weight*b_size, net_height*b_size);
+            for (int m_n = 0; m_n <= 8; m_n++) 
+            {Pictures_array[m_n] = Image.FromFile("C:/Users/kuzak/source/repos/Techies/Techies/Properties/"+ m_n.ToString()+ "_m.png");}
+            this.ClientSize = new Size(net_weight*b_size + indents*2, net_height*b_size + indents*2);
+            this.FormBorderStyle = FormBorderStyle.FixedDialog;
+            this.StartPosition = FormStartPosition.CenterScreen;
+            this.MaximizeBox = false;
             for (int i = 0 ; i <net_weight;  i++)
             {
                 for (int j = 0 ; j < net_height; j++)
